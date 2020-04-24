69b3baf9a619da1e8ba15441714034a12ddaddf8
Matthew Warren <matthewwarren101010@gmail.com>
2020-04-24 01:18:44 -0500

    Switch from JavaScript to CSS for keeping the footer at the bottom

--- site.js
+++ site.js
@@ -5,16 +5,3 @@ window.addEventListener('keyup', function(e) {
     document.querySelector('html').classList.toggle('g');
   }
 })
-
-if(document.querySelector('body').offsetHeight < window.innerHeight) {
-	document.querySelector('footer').style = "position: absolute; bottom: 0; width: 100%;"
-}
-
-window.addEventListener('resize', function(e) {
-	if(document.querySelector('body').offsetHeight < window.innerHeight) {
-		document.querySelector('footer').style = "position: absolute; bottom: 0; width: 100%;"
-	}
-	else {
-		document.querySelector('footer').style = ""
-	}
-})
