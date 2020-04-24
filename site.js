cb53de88b2924404e88280b525aaf77603be1a98
Matthew Warren <matthewwarren101010@gmail.com>
2020-04-24 01:26:33 -0500

    Remove unnecessary log to console

--- site.js
+++ site.js
@@ -1,5 +1,4 @@
 window.addEventListener('keyup', function(e) {
-  console.log(e.keyCode);
   // Toggle the visibility of gridlines when `g` is pressed
   if (e.keyCode === 71) {
     document.querySelector('html').classList.toggle('g');
